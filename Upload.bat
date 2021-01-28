d8b3d316d170a44b3d7a43eec8f6cc8548aba99d
YoruTen <andreas.gustavsson92@gmail.com>
2021-01-28 13:16:52 +0100

    Delete Upload.bat

--- Upload.bat
+++
@@ -1,7 +0,0 @@
-
-ampy --port COM5 put src\main.py
-ampy --port COM5 put src\config.json
-ampy --port COM5 put src\lib\imu.py /lib/imu.py
-ampy --port COM5 put src\lib\beep.py /lib/beep.py
-ampy --port COM5 put src\lib\Kalman.py /lib/Kalman.py
-ampy --port COM5 put src\lib\urequests.py /lib/urequests.py
